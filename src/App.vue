32a9eaeb7e60931e7b554042f8d9dfcf8e8ceaac
timcharlier6 <128478437+timcharlier6@users.noreply.github.com>
2023-10-31 16:58:15 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>Food</h1>
+  <div id="wrapper">
+    <food-item
+        food-name="Apples"
+        food-desc="Apples are fruits."
+        v-bind:is-favorite="true"/>
+    <food-item
+        food-name="Pizza"
+        food-desc="Pizza are from Italy."
+        v-bind:is-favorite="false"/>
+
+    <food-item
+        food-name="Something else"
+        v-bind:is-favorite="false"/>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+#wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+#wrapper > div {
+  border: dashed black 1px;
+  margin: 10px;
+  padding: 10px;
+  background-color: lightgreen;
+}
+</style>
---
+++ src/components/FoodItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <div v-on:click="countClicks">
+    <h2>
+      {{ foodName }}
+      <img src="./../assets/vue.svg" v-show="isFavorite" alt="img">
+    </h2>
+    <p>{{ foodDesc }}</p>
+    <p id="red">Number of clicks : {{ clicks }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    data() {
+      return {
+        clicks: 0
+      }
+    },
+    // props: ['foodName','foodDesc','isFavorite']
+    props: {
+      foodName: {
+        type: String,
+        required: true
+      },
+      foodDesc: {
+        type: String,
+        required: false,
+        default: 'This is a default description.'
+      },
+      isFavorite: {
+        type: Boolean,
+        required: false,
+        default: false
+      }
+    },
+    methods: {
+      countClicks() {
+        this.clicks++;
+      }
+    }
+  }
+</script>
+
+<style>
+  #red {
+    font-weight: bold ;
+    color: rgb(144, 12, 12);
+  }
+
+  img {
+    height: 1.5em;
+    float: right;
+  }
+</style>
